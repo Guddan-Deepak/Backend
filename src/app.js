9d78c3dcef4b61b6e3530e32d6ac5cfad7e04aa1
Guddan-Deepak <deepakdesh565@gmail.com>
2025-10-02 11:24:13 +0530

    all setup configer

--- src/app.js
+++ src/app.js
@@ -16,4 +16,16 @@ app.use(express.json({limit: "16kb"}))  // url se jo data aata hai json format m
 app.use(express.urlencoded({extended: true,limit: "16kb"}))       // parses form data
 app.use(express.static("public")) // serves static files
 app.use(cookieParser())     // parses cookies
+
+
+
+//routes
+import userRouter from "./routes/user.router.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)     //https://localhost:8000/api/v1/users/register
+
+
+
 export { app }
